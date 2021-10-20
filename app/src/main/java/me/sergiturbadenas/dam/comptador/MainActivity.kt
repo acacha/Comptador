5c4b7fe1d1fd2530a953f7a2dc2bae861dca0ad5
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2021-10-20 16:20:41 +0200

    Primera versió del timer

--- app/src/main/java/me/sergiturbadenas/dam/comptador/MainActivity.kt
+++ app/src/main/java/me/sergiturbadenas/dam/comptador/MainActivity.kt
@@ -2,8 +2,10 @@ package me.sergiturbadenas.dam.comptador
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,33 +13,63 @@ class MainActivity : AppCompatActivity() {
     internal lateinit var timeTextView : TextView
     internal lateinit var counterTextView : TextView
     internal var counter = 0
-    internal var time = 60
+    internal var time = 10
+
+    internal var appStarted = false
+    internal lateinit var countdownTimer : CountDownTimer
+    internal val initialCountDownTimer: Long = 60000
+    internal val intervalCountDownTimer: Long = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initCountdown()
+
         tapMeButton = findViewById(R.id.tapMeButton)
         timeTextView = findViewById(R.id.timeTextView)
         counterTextView = findViewById(R.id.counterTextView)
 
-        // Actualitzar o definir valor inicial de counterTextView -> counterTextView = score -> 0
-
-        // TODO en algun moment haurem d'executar incrementCounter
-
         tapMeButton.setOnClickListener {
+            if (!appStarted) {
+                startGame()
+            }
             incrementCounter()
-            // TODO -> Iniciar el comptador
         }
-
-//        timeTextView.text = time.toString()
         timeTextView.text = getString(R.string.timeText, time)
+    }
 
+    private fun startGame() {
+        countdownTimer.start()
+        appStarted = true
+    }
+
+    private fun initCountdown() {
+        countdownTimer = object : CountDownTimer(initialCountDownTimer,intervalCountDownTimer) {
+            override fun onTick(millisUntilFinished: Long) {
+                val timeLeft = millisUntilFinished / 1000
+                timeTextView.text = timeLeft.toString()
+            }
+
+            override fun onFinish() {
+                endGame()
+            }
+        }
     }
 
     private fun incrementCounter() {
         //        counter = counter +1
         counter += 1
         counterTextView.text = counter.toString()
+    }
+
+    private fun endGame() {
+        Toast.makeText(this, getString(R.string.endGame), Toast.LENGTH_LONG).show()
+//        resetGame()
+    }
+
+    private fun resetGame() {
+        TODO("Not yet implemented")
+
     }
 }
